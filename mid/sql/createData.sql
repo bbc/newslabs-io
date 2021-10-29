19c59a46cecbc55ecb05a9e74c40434a72693dbe
Nik Rahmel <nik@rahmel.eu>
2021-10-29 15:04:55 +0100

    Task: Add location data

--- mid/sql/createData.sql
+++ mid/sql/createData.sql
@@ -13,11 +13,12 @@ VALUES(2, 'Aisha', 2);
 INSERT INTO users(id, username, role_id)
 VALUES(3, 'Kyra', 2);
 
-INSERT INTO submissions(user_id, title, text)
-VALUES(2, 'Tree in Birmingham', 'It fell over!');
+INSERT INTO submissions(user_id, title, text, location)
+VALUES(2, 'Tree in Birmingham', 'It fell over!', '52.4862,1.8904');
+
 
 INSERT INTO submissions(user_id, title, text)
 VALUES(2, 'Tree in Birmingham 2', 'It''s back up');
 
-INSERT INTO submissions(user_id, title, text)
-VALUES(3, 'Zoom Hacking in the Lords', 'I have some details about how the Lords have been hacked!');
+INSERT INTO submissions(user_id, title, text, location)
+VALUES(3, 'Zoom Hacking in the Lords', 'I have some details about how the Lords have been hacked!', '55.7558,37.6173');
