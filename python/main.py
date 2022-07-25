656fe29965568344b568aac17d469b05a8f41fd1
Ben Nuttall <ben@bennuttall.com>
2022-07-25 17:05:26 +0100

    Allow multi-origin access (CORS)

--- python/main.py
+++ python/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db import get_submissions_and_users, get_submission_and_user, get_users_and_roles
 from models import Submission, User
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/users")
